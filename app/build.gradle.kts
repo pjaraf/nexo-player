4ab26e988571f0dca05fd5042980c128934e9710
pjaraf <143246360+pjaraf@users.noreply.github.com>
2026-08-17 04:21:35 +0200

    V 1.1.5 .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aistudio.nexus.jqvzkp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.1.4"
+        versionCode = 16
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
