43f07b401037bb69452ada4d7b93e6b68965427a
pjaraf <143246360+pjaraf@users.noreply.github.com>
2026-08-17 07:32:24 +0200

    Solución

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aistudio.nexus.jqvzkp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 25
-        versionName = "1.1.14"
+        versionCode = 26
+        versionName = "1.1.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
