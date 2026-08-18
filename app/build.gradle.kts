432400e001437a62803c2a3ca60a09ca7c7a94b6
pjaraf <143246360+pjaraf@users.noreply.github.com>
2026-08-18 05:44:19 +0200

    Ojalas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nexo.player"
         minSdk = 24
         targetSdk = 35
-        versionCode = 36
-        versionName = "1.1.25"
+        versionCode = 37
+        versionName = "1.1.26"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
