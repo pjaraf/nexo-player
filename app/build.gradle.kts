dee82310212277c399449e311dd56570df3dc3dc
pjaraf <143246360+pjaraf@users.noreply.github.com>
2026-08-25 13:47:53 +0200

    2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nexo.player"
         minSdk = 24
         targetSdk = 35
-        versionCode = 133
-        versionName = "1.2.22"
+        versionCode = 134
+        versionName = "1.2.23"
 
         ndk {
             abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
